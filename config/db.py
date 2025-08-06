86525dbb874963a71ca4479e7f20780f354fd50e
Chitravansh <mchitravansh@gmail.com>
2025-08-06 23:24:53 +0530

    first commit

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://chitravanshmohandevelops:8787230617@cluster0.est0t.mongodb.net"
+MONGO_URI = "mongodb://localhost:27017/"
 
 conn = MongoClient(MONGO_URI)
 
